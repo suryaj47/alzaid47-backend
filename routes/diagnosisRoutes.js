d097fcf02b775e016a62ce368b2bebaadac2da64
suryajp47 <suryaj4747@gmail.com>
2026-03-19 09:34:49 +0530

    debuging

--- routes/diagnosisRoutes.js
+++ routes/diagnosisRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const Diagnosis = require("../models/Diagnosis");
 const verifyToken = require("../middleware/authMiddleware");
 
 const { savetestresult, getdiagnosis,history } = require("../controllers/diagnosisController");
 router.post("/save", verifyToken, savetestresult);
 router.get("/latest", verifyToken, getdiagnosis);
 router.get("/history", verifyToken, history);
+
+module.exports = router;
